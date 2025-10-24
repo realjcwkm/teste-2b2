65daf03567d9688a36c523dabc57ff80af78554d
realjcwkm <43896606+realjcwkm@users.noreply.github.com>
2025-10-24 09:13:37 -0400

    criando o projeto inicial

---
+++ src/main/java/com/mycompany/teste/b2/Teste2b2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste.b2;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Teste2b2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
